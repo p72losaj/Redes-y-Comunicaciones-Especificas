7e2a44cfd9304f4a4dc41de6b191406669a67832
Jaime Lorenzo Sanchez <jlorenzosa@gmail.com>
2022-03-04 10:23:55 +0100

    Finalizacion practica0

--- practica0/practica0.ino
+++ practica0/practica0.ino
@@ -1,25 +1,46 @@
-/*******************************************************
+/***********
 * Redes y Comunicaciones Especificas
 * Practica 0
+*   1- Cargar en una variable tipo byte, el valor capturado por las entradas digitales 0 al 7, 
+*      siendo el puerto de entrada digital 0 el menos significativo y el puerto de entrada 7 el más significativo.
+*   2- Pausar entre una carga y otra de la variable tipo byte q se captura.
 * Realizado por Jaime Lorenzo Sanchez
 ********************************************************/
 
+// Pin de entrada -> Leemos la informacion
+const int entrada[] = {2,3,4,5,6,7,8,9};
+// Pin de salida -> Sacamos la informacion
+const int salida[] = {10,11,12,13,A0,A1,A2,A3};
+byte valores = 0;
+byte contador = 0;
+
 void setup() {
-  Serial.begin(9600); // Inicializamos el serial con un ratio de 9600 bps
-  while(!Serial){; } // Necesario para el uso de Serial
+  // Inicializamos el terminal
+  Serial.begin(9600);
+  // Necesario para el uso de Serial 
+  while(!Serial){; } 
+  // Inicializamos los pin de entrada y salida
+  for(int i=0; i < 8; i++){
+    pinMode(entrada[i], INPUT_PULLUP);
+    pinMode(salida[i],OUTPUT);
+  }
 }
 
 void loop () {
-  // Mostramos los puertos y su valor
-  Serial.println("Mostrando los puertos y su valor, del menos significativo al mas significativo");
-  for(int i=0; i < 8; i++){
-    // Mostramos el numero de puerto
-    Serial.print("Puerto: ");
-    Serial.print(i);
-    byte valor = digitalRead(i); // Cargamos en una variable tipo byte el valor obtenido por la entrada digital
-    // Mostramos el valor cargado del puerto en un formato binario
-    Serial.print(" -> Valor del puerto: ");
-    Serial.println(valor);
-    delay(900); // Espera de 900 ms
+  // Aumentamos el contador
+  contador++;
+  // Escribimos el valor del contador en los pin de salida
+  for(int i=0; i<8; i++){
+    digitalWrite(salida[i], bitRead(contador,i));
   }
 }
+// Escribimos los valores de entrada en la variable de entrada
+for(int i=0; i<8; i++){
+  bitWrite(entrada,i,digitalRead(entrada[i]));
+}
+// Imprimimos los valores por terminal
+for(int i=0; i < 8; i++){
+  Serial.println(bitRead(entrada,7-i), BIN);
+}
+// Realizamos una espera de 1 segundo
+delay(1000);
--- practica2/practica2.ino
+++ practica2/practica2.ino
@@ -15,41 +15,7 @@
   * se debe engregar mediante líneas de comentario.
   */
 
-int opcion; // Opcion del menu
-int puerto;
-   
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); // Inicializamos el serial con un ratio de 9600 bps
-  while(!Serial){;} // Necesario para el uso de Serial
-  
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  menu();
-  while(opcion<49 || opcion > 51){
-    if(Serial.available()){
-      opcion = Serial.read();
-    }  
-  }
-  switch(opcion){
-    case '1':
-      Serial.print("Selecciona puerto: ");
-      while(puerto<48 || puerto>57){
-          if(Serial.available() > 0){puerto = Serial.read();}  
-  }
-    break;
-  }
-}
-
-void menu(){
-  Serial.print("\n");
-  Serial.println("(1) Obtener el canal de entrada");
-  Serial.print("Introduce una opcion: ");
-}
-
-  /*
+/*
    * Nota 1: Cambiar la frecuencia de la señal periódica y verificar si la conversión A/D te proporciona una correcta 
    * resolución de la señal convertida. Probar con 50Hz, 100 Hz, 500 Hz, 1000 Hz, 1500 Hz, 3000Hz y 5000 Hz. 
    * Analizar los datos y responder a las siguiente pregunta  ¿Qué ocurre en cada uno de estos casos? 
@@ -57,3 +23,36 @@ void menu(){
    * Nota 2: Utilizar el mismo número de muestras al tener en cuenta la nota 1. 
    * El número de muestras se debe proporcionar a través del monitor.
    */
+
+int* muestras; // Vector de muestras
+const int pinEntrada = A0; 
+int nLecturas = 0;
+   
+void setup() {
+  // Inicializamos el terminal
+  Serial.begin(9600); 
+  // Inicialiamos el pin de entrada
+  pinMode(pinEntrada,INPUT);
+}
+
+void loop() {
+  // Leemos el numero de lecturas
+  Serial.println("Introduce el numero de lecturas: ");
+  nLecturas = Serial.readString().toInt(); // La cadena obtenida la transformamos en un entero
+  if(nLecturas != 0){
+    // Realizamos una reserva de memoria del vector de muestras
+    muestras = (int*) malloc (sizeof(int)* nLecturas);
+    // Lectura del pin de entrada
+    for(int i=0; i<nLecturas; i++){
+      muestras[i] = analogRead(pinEntrada);
+    }
+    // Imprimimos los resultados obtenidos
+    for(int i=0; i< nLecturas; i++){
+      Serial.println(muestras[i]);
+    }
+    // Liberamos la memoria del vector de muestras
+    free(muestras);
+  }
+}
+
+// Resultados obtenidos

f83e6af1834151c6752db56d73682e277b0aa18f
Jaime Lorenzo Sanchez <jlorenzosa@gmail.com>
2022-04-04 14:10:52 +0200

    Obtencion muestras practica2

--- practica2/practica2.ino
+++ practica2/practica2.ino
@@ -3,56 +3,86 @@
  * Practica 2. Conversion A/D mediante Arduino
  * Realizado por Jaime Lorenzo Sanchez
  */
- 
- /*
-  * Crear un unico programa que permita realizar las siguientes tareas  
-  * 1. El canal de entrada (puerto analógico) y el número de muestras se debe proporcionar a tráves del monitor serie.
-  * 2. Conversion A/D de senal analogica: la señal analógica será proporcionada por el generador de funciones 
-  * (señal senoidal de 5 voltios de amplitud y una fecuencia de 1000 Hz)
-  * 3. Guardar los datos convertidos en un vector de un tamaño que puede ser variable valores/muestras 
-  * (el número de muestras se debe pedir).
-  * 4. Realizar el mismo proceso para una señal triangular, y comentar los resultados en el fichero -ino que 
-  * se debe engregar mediante líneas de comentario.
-  */
 
-/*
-   * Nota 1: Cambiar la frecuencia de la señal periódica y verificar si la conversión A/D te proporciona una correcta 
-   * resolución de la señal convertida. Probar con 50Hz, 100 Hz, 500 Hz, 1000 Hz, 1500 Hz, 3000Hz y 5000 Hz. 
-   * Analizar los datos y responder a las siguiente pregunta  ¿Qué ocurre en cada uno de estos casos? 
-   * "Indicarlo en el fichero .ino que se entraga mediante líneas comentadas"
-   * Nota 2: Utilizar el mismo número de muestras al tener en cuenta la nota 1. 
-   * El número de muestras se debe proporcionar a través del monitor.
-   */
-
-int* muestras; // Vector de muestras
-const int pinEntrada = A0; 
-int nLecturas = 0;
+// Vector de muestras
+int* muestras; 
+// Pin analogico
+int pin_analogico = 0;
    
 void setup() {
   // Inicializamos el terminal
   Serial.begin(9600); 
-  // Inicialiamos el pin de entrada
-  pinMode(pinEntrada,INPUT);
+  while(!Serial){;}
 }
 
-void loop() {
-  // Leemos el numero de lecturas
-  Serial.println("Introduce el numero de lecturas: ");
-  nLecturas = Serial.readString().toInt(); // La cadena obtenida la transformamos en un entero
-  if(nLecturas != 0){
-    // Realizamos una reserva de memoria del vector de muestras
-    muestras = (int*) malloc (sizeof(int)* nLecturas);
-    // Lectura del pin de entrada
-    for(int i=0; i<nLecturas; i++){
-      muestras[i] = analogRead(pinEntrada);
+
+void loop(){
+  // Numero de muestras a proporcionar a través del monitor serie
+  int lecturas = 0;
+  // El número de muestras se debe proporcionar a tráves del monitor serie.
+  Serial.print("Introduce el numero de lecturas: ");
+  // Esperamos a que el usuario introduzca el numero de muestras
+  while(Serial.available() == 0){;}
+  lecturas = Serial.readString().toInt(); // La cadena obtenida la transformamos en un entero
+  // El numero de lecturas debe ser distinto a 0
+  Serial.println(lecturas);
+  if(lecturas != 0){
+    // Reservamos memoria para el vector de muestras
+    muestras = (int*) malloc (sizeof(int)* lecturas);
+    // El canal de entrada (puerto analógico) se debe proporcionar a través del monitor serie
+    Serial.print("Introduce el canal de entrada (pin analogico): ");
+    // Esperamos a que el usuario introduzca el puerto de entrada
+    while(Serial.available() == 0){;}
+    pin_analogico = Serial.readString().toInt();
+    Serial.println(pin_analogico);
+    // Leemos el numero de muestras del pin
+    for(int i=0; i<lecturas; i++){
+      muestras[i] = analogRead(pin_analogico);
     }
     // Imprimimos los resultados obtenidos
-    for(int i=0; i< nLecturas; i++){
-      Serial.println(muestras[i]);
+    Serial.println("Mostrando los resultados obtenidos");
+    for(int i=0; i<lecturas; i++){
+      // La senal analogica sera proporcionada por el generador de funciones
+      float voltaje = muestras[i] * (5.0 / 1023.0);
+      Serial.print(muestras[i]);
+      Serial.print(" || ");
+      Serial.println(voltaje);
+      
     }
-    // Liberamos la memoria del vector de muestras
+    // Liberamos la memoria del vector
     free(muestras);
   }
 }
 
 // Resultados obtenidos
+
+/*
+ * 
+ * Nota 1: Cambiar la frecuencia de la señal periódica y verificar si la conversión A/D te proporciona una correcta 
+ * resolución de la señal convertida. Probar con 50Hz, 100 Hz, 500 Hz, 1000 Hz, 1500 Hz, 3000Hz y 5000 Hz. 
+ * Analizar los datos y responder a las siguiente pregunta  ¿Qué ocurre en cada uno de estos casos? 
+ * Nota 2: Utilizar el mismo número de muestras al tener en cuenta la nota 1. 
+ * El número de muestras se debe proporcionar a través del monitor.
+ */
+
+ /*
+  * Analisis de los datos por Jaime Lorenzo Sanchez
+  * 1) Señal senoidal de 5 voltios de amplitud y una fecuencia de 1000 Hz -> Ver las graficas del directorio senoidal
+  * 1 hz -> Imagen2
+  * 50 Hz -> Imagen4
+  * 100 Hz -> Imagen5
+  * 1khz -> Imagen1
+  * 1500 Hz -> Imagen6
+  * 3000 Hz -> Imagen7
+  * 5000 Hz -> Imagen8
+  * 10 Khz -> Imagen3
+  * 2) Realizar el mismo proceso para una señal triangular
+  * 1 hz -> Imagen9
+  * * 50 Hz -> Imagen10
+  * 100 Hz -> Imagen11
+  * 1khz -> Imagen12
+  * 1500 Hz -> Imagen13
+  * 3000 Hz -> Imagen14
+  * 5000 Hz -> Imagen15
+  * 10 Khz -> Imagen16
+  */
